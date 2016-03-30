574a8c5c2b04890fa7fe3ddee96f92e326e35a76
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-03-30 13:16:23 +1000

    Fixed tests

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -4,6 +4,7 @@ echo "Testing branch: ${CI_BRANCH}"
 
 if [ ${PIPE_NUM} == "1" ]; then
     # Run local tests
+    npm install bower web-component-tester -g
     echo "Starting local WCT tests"
     bower install
     wct
